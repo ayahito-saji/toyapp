02b2414c5c68255cdba631468aaed0a2202f538c
Saji Ayahito <saji.ayahito@h.mbox.nagoya-u.ac.jp>
2017-12-03 16:54:36 +0900

    rails generate scaffold micropost content:text user_id:integer rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171203073515) do
+ActiveRecord::Schema.define(version: 20171203075156) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
